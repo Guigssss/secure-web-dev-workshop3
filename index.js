34b3baf36dc6220d799b479a339ca98a10a1edfe
Guigssss <guillaume.mouty@gmail.com>
2022-11-14 12:16:47 +0100

    feat (3) : Mongoose connection

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const locationController = require('./locations/locations.controller')
 const app = express()
 const port = 3000
 
+require('dotenv').config()
 
-app.use(locationController)
+const mongoose = require('mongoose');
+main();
 
+async function main() {
+	await mongoose.connect(process.env.MONGO_URI)
+	console.log("Well done")
 
-app.listen(port, () => {
-	console.log(`API listening on port ${port}, visit http://localhost:${port}/`)
-})
+	app.use(locationController)
+
+	app.listen(port, () => {
+		console.log(`API listening on port ${port}, visit http://localhost:${port}/`)
+	})
+}
